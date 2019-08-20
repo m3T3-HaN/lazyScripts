23bf8a60744fc6e60d50ad5d6e99e86ccc47da55
Metehan BOY <m3T3-HaN@hotmail.com>
2019-08-20 12:44:44 +0300

    array input updating
    scripts input convert to array

--- lazyload.js
+++ lazyload.js
@@ -1,27 +1,88 @@
-            function lazyScripts (type,url,path="") {
-                let tester = url.indexOf('http') !== -1 ? new RegExp(url,'i') : new RegExp(path+url,'i');
-                if(type == "js"){
-                    if(Array.from(document.scripts).filter(x => tester.test(x.src)).length > 0)
-                        return;
-                }else{
-                    if(Array.from(document.styleSheets).filter(x => tester.test(x.href)).length > 0)
-                        return;
+            function lazyScripts (scriptArray,path="") {
+
+                var pendingScripts = [];
+                var firstScript = document.scripts[0];
+
+                function stateChange() {
+                    var pendingScript;
+                    while (pendingScripts[0] && pendingScripts[0].readyState == 'loaded') {
+                        pendingScript = pendingScripts.shift();
+                        pendingScript.onreadystatechange = null;
+                        firstScript.parentNode.insertBefore(pendingScript, firstScript);
+                    }
                 }
-                let s = type == "js" ? document.createElement('script') : document.createElement('link');
-                s.type = type == "js" ? 'text/javascript' : 'text/css';
-                if(type == 'css') s.rel = 'stylesheet';
-                if(type == 'js')
-                    s.src = url.indexOf('http') !== -1 ? url : path+url;
-                else
-                    s.href= url.indexOf('http') !== -1 ? url : path+url;
-                let x = document.getElementsByTagName('head')[0];
-                x.appendChild(s);
+
+                Object.keys(dizi).forEach((type) => {
+                    while(src = dizi[type].shift()){
+
+                        let tester = src.indexOf('http') !== -1 ? new RegExp(src,'i') : new RegExp(path+src,'i');
+                        if(type == "js"){
+                            if(Array.from(document.scripts).filter(x => tester.test(x.src)).length > 0){
+                                continue;
+                            }
+                            else if ('async' in firstScript) {
+                                script = document.createElement('script');
+                                script.async = false;
+                                script.src = src.indexOf('http') !== -1 ? src : path+src;
+                                firstScript.parentNode.insertBefore(script, firstScript);
+                            }
+                            else if (firstScript.readyState) {
+                                script = document.createElement('script');
+                                pendingScripts.push(script);
+                                script.onreadystatechange = stateChange;
+                                script.src = src.indexOf('http') !== -1 ? src : path+src;
+                            }
+                            else {
+                                document.write('<script src="' + src + '" defer></'+'script>');
+                            }
+                        }else{
+                            if(Array.from(document.styleSheets).filter(x => tester.test(x.href)).length > 0){
+                                continue;
+                            }
+                            else{
+                                script = document.createElement('link');
+                                script.rel = "stylesheet";
+                                script.type = "text/css";
+                                script.href = src.indexOf('http') !== -1 ? src : path+src;
+                                firstScript.parentNode.insertBefore(script, firstScript);
+                            }
+                        }
+
+                    }
+                });
+
+            }
             }
 
 
 //example usage:
-            //for remote links;  
-            lazyScripts('js','https://cdn.jsdelivr.net/npm/vue');
+            /*
+            object name is not importtant but object key name js and css must declared
+            
+            */
+            let assets = {
+               "js":[
+                        "js/plugins/lists.min.js",
+                        "js/plugins/paragraph_format.min.js",
+                        "js/plugins/paragraph_style.min.js",
+                        "js/plugins/quick_insert.min.js",
+                        "js/plugins/quote.min.js",
+                        "js/plugins/table.min.js",
+                        "js/plugins/save.min.js",
+                        "js/plugins/url.min.js",
+               ],
+               "css":[
+                        "css/plugins/image_manager.css",
+                        "css/plugins/image.css",
+                        "css/plugins/line_breaker.css",
+                        "css/plugins/table.css",
+                        "css/plugins/char_counter.css",
+               ]
+            };
+            //for lazy links without path;  
+            lazyScripts(assets);
 
-            //for local path;
-            lazyScripts('js','vue.min.js','/assets/js/');
+
+            //for lazy links wit path;  
+            lazyScripts(assets,"/public/bower_components/");
+
